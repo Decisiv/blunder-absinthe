3009091ff8115197c4479da77133c363f7a2634f
Michael Lagutko <mlagutko@decisiv.net>
2018-05-01 16:26:43 +0300

    [MICAEG-290] original error is used to report to bugsnag ony when it exist

--- test/blunder/absinthe/error_handler/bug_snag_test.exs
+++ test/blunder/absinthe/error_handler/bug_snag_test.exs
@@ -5,11 +5,21 @@ if Code.ensure_loaded?(Bugsnag) do
 
     import Mock
 
-    test_with_mock "reports to Bugsnag", Bugsnag, [report: fn (_, _) -> :ok end] do
-      blunder = %Blunder{severity: :error, code: :code}
+    test_with_mock "reports to Bugsnag with an original error when original error exists",
+                   Bugsnag, [report: fn (_, _) -> :ok end] do
+      blunder = %Blunder{severity: :error, code: :code, original_error: "unexpected"}
       Blunder.Absinthe.ErrorHandler.BugSnag.call(blunder)
       assert called Bugsnag.report(
         blunder.original_error, context: :code, metadata: :_, severity: "error", stacktrace: blunder.stacktrace
+      )
+    end
+
+    test_with_mock "reports to Bugsnag with an blunder error when original error does not exist",
+                   Bugsnag, [report: fn (_, _) -> :ok end] do
+      blunder = %Blunder{severity: :error, code: :code}
+      Blunder.Absinthe.ErrorHandler.BugSnag.call(blunder)
+      assert called Bugsnag.report(
+        blunder, context: :code, metadata: :_, severity: "error", stacktrace: blunder.stacktrace
       )
     end
   end
